8a6ddaa12b339b6a0f72af5e8ed351db82f42ee4
codezen-ashish <ixtashish@gmail.com>
2025-05-22 12:39:44 +0530

    fix path

---
+++ tic-tac-Game/app.js
@@ -0,0 +1,6 @@
+function startGame() {
+      document.getElementById('mainContainer').classList.add('hide');
+      document.getElementById('playBoard').classList.add('show');
+      document.querySelector('.replayBtn').style.display = 'noun';
+    }
+
